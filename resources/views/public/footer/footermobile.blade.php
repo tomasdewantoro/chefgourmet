938bc20e3dea0e7951b81d5fae98e8d21c9d8f73
Tomas Dewantoro <tomas.dewantoro@gmail.com>
2019-08-28 20:09:15 +0700

    separated footer

---
+++ resources/views/public/footer/footermobile.blade.php
@@ -0,0 +1,17 @@
+<footer>
+    <div class="col-md-12 section-space text-center">
+        <h6 class="font-weight-bold mb-0">Contact Us:</h6>
+        <p class="font-italic">JL. Asem Jaya VII No.3 Surabaya, Jawa Timur</p>
+        <h6 class="font-weight-bold mb-0">Email:</h6>
+        <p class="font-italic">project@gourmetchef.id</p>
+        <h6 class="font-weight-bold mb-0">Phone:</h6>
+        <p class="font-italic">0812 7689 1513</p>
+    </div>
+    <div class="social-icons-mobile d-flex justify-content-center px-5">
+        <a href="https://whatsapp.com"><img src="{{ asset('/images/whatsapp2.png') }}" width="100%" alt=""></a>
+        <a href="https://www.instagram.com/gourmetchef_counsultant/"><img src="{{ asset('/images/instagram.png') }}" width="100%" alt=""></a>
+        <a href="https://facebook.com"><img src="{{ asset('/images/facebook.png') }}" width="100%" alt=""></a>
+    </div>
+    <img src="{{ asset('/images/map.jpg') }}" width="100%" alt="">
+    <span class="d-block text-center font-italic font-weight-light mx-2 my-2">Gourmet Chef © 2019.All right reserved.<br/> Developed by <a target="_blank" href="https://codein.id" class="text-color font-weight-bold">Codein.ID</a></span>
+</footer>
